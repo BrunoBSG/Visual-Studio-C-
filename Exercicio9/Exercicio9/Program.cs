8ed1888d758ddfd634f1dc7a638c47ffe26dbf5e
BrunoBSG <bsimoesgoulart@gmail.com>
2025-10-02 14:34:49 -0300

    Exercicio9

---
+++ Exercicio9/Exercicio9/Program.cs
@@ -0,0 +1,12 @@
+﻿int m  = 0;
+double valorcompra = 0;
+Console.WriteLine("Digite o número de maçãs que você comprou");
+m =int.Parse(Console.ReadLine());
+if (m<=12) {
+    valorcompra = m;
+    Console.WriteLine($"Sua compra ficou R${valorcompra}") ; }
+else if (m>12) {
+    
+    valorcompra = m * 1.30;
+    Console.WriteLine($"Sua compra ficou R${valorcompra}"); }
+ 
