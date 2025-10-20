2e75baa3185b2c78e65c42f9facea6730e78380e
BrunoBSG <bsimoesgoulart@gmail.com>
2025-10-20 17:25:48 -0300

    Terminar

---
+++ Video/Video/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Video
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panellogo = new System.Windows.Forms.Panel();
+            this.btnProducts = new System.Windows.Forms.Button();
+            this.buttonOrders = new System.Windows.Forms.Button();
+            this.buttonCustumer = new System.Windows.Forms.Button();
+            this.buttonReporting = new System.Windows.Forms.Button();
+            this.buttonNotifications = new System.Windows.Forms.Button();
+            this.buttonsettings = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panel1.Controls.Add(this.buttonsettings);
+            this.panel1.Controls.Add(this.buttonNotifications);
+            this.panel1.Controls.Add(this.buttonReporting);
+            this.panel1.Controls.Add(this.buttonCustumer);
+            this.panel1.Controls.Add(this.buttonOrders);
+            this.panel1.Controls.Add(this.btnProducts);
+            this.panel1.Controls.Add(this.panellogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 548);
+            this.panel1.TabIndex = 0;
+            // 
+            // panellogo
+            // 
+            this.panellogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panellogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panellogo.Location = new System.Drawing.Point(0, 0);
+            this.panellogo.Name = "panellogo";
+            this.panellogo.Size = new System.Drawing.Size(220, 75);
+            this.panellogo.TabIndex = 0;
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProducts.FlatAppearance.BorderSize = 0;
+            this.btnProducts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProducts.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnProducts.Image = global::Video.Properties.Resources.icons8_carrinho_32;
+            this.btnProducts.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProducts.Location = new System.Drawing.Point(0, 75);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnProducts.Size = new System.Drawing.Size(220, 60);
+            this.btnProducts.TabIndex = 1;
+            this.btnProducts.Text = "Products";
+            this.btnProducts.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnProducts.UseVisualStyleBackColor = true;
+            // 
+            // buttonOrders
+            // 
+            this.buttonOrders.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonOrders.FlatAppearance.BorderSize = 0;
+            this.buttonOrders.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOrders.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonOrders.Image = global::Video.Properties.Resources.icons8_passar_24;
+            this.buttonOrders.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonOrders.Location = new System.Drawing.Point(0, 135);
+            this.buttonOrders.Name = "buttonOrders";
+            this.buttonOrders.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonOrders.Size = new System.Drawing.Size(220, 60);
+            this.buttonOrders.TabIndex = 2;
+            this.buttonOrders.Text = "Orders";
+            this.buttonOrders.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonOrders.UseVisualStyleBackColor = true;
+            // 
+            // buttonCustumer
+            // 
+            this.buttonCustumer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonCustumer.FlatAppearance.BorderSize = 0;
+            this.buttonCustumer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCustumer.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonCustumer.Image = global::Video.Properties.Resources.icons8_carrinho_32;
+            this.buttonCustumer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonCustumer.Location = new System.Drawing.Point(0, 195);
+            this.buttonCustumer.Name = "buttonCustumer";
+            this.buttonCustumer.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonCustumer.Size = new System.Drawing.Size(220, 60);
+            this.buttonCustumer.TabIndex = 3;
+            this.buttonCustumer.Text = "Custumer";
+            this.buttonCustumer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonCustumer.UseVisualStyleBackColor = true;
+            // 
+            // buttonReporting
+            // 
+            this.buttonReporting.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonReporting.FlatAppearance.BorderSize = 0;
+            this.buttonReporting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonReporting.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonReporting.Image = global::Video.Properties.Resources.icons8_carrinho_32;
+            this.buttonReporting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonReporting.Location = new System.Drawing.Point(0, 255);
+            this.buttonReporting.Name = "buttonReporting";
+            this.buttonReporting.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonReporting.Size = new System.Drawing.Size(220, 60);
+            this.buttonReporting.TabIndex = 4;
+            this.buttonReporting.Text = "Reporting";
+            this.buttonReporting.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonReporting.UseVisualStyleBackColor = true;
+            // 
+            // buttonNotifications
+            // 
+            this.buttonNotifications.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNotifications.FlatAppearance.BorderSize = 0;
+            this.buttonNotifications.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNotifications.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonNotifications.Image = global::Video.Properties.Resources.icons8_campainha_24;
+            this.buttonNotifications.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonNotifications.Location = new System.Drawing.Point(0, 315);
+            this.buttonNotifications.Name = "buttonNotifications";
+            this.buttonNotifications.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonNotifications.Size = new System.Drawing.Size(220, 60);
+            this.buttonNotifications.TabIndex = 5;
+            this.buttonNotifications.Text = "Notifications";
+            this.buttonNotifications.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonNotifications.UseVisualStyleBackColor = true;
+            // 
+            // buttonsettings
+            // 
+            this.buttonsettings.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonsettings.FlatAppearance.BorderSize = 0;
+            this.buttonsettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonsettings.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonsettings.Image = global::Video.Properties.Resources.icons8_settings_32;
+            this.buttonsettings.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonsettings.Location = new System.Drawing.Point(0, 375);
+            this.buttonsettings.Name = "buttonsettings";
+            this.buttonsettings.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonsettings.Size = new System.Drawing.Size(220, 60);
+            this.buttonsettings.TabIndex = 6;
+            this.buttonsettings.Text = "Setting";
+            this.buttonsettings.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonsettings.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 548);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button btnProducts;
+        private Panel panellogo;
+        private Button buttonsettings;
+        private Button buttonNotifications;
+        private Button buttonReporting;
+        private Button buttonCustumer;
+        private Button buttonOrders;
+    }
+}
